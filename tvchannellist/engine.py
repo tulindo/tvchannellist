05087470d04e45efae38758317a7f287cc4afd1e
Paolo Tuninetto <paolo.tuninetto@gmail.com>
2020-02-25 22:16:45 +0100

    engine

--- tvchannellist/engine.py
+++ tvchannellist/engine.py
@@ -1,8 +1,10 @@
 """The TV Channel List module."""
 from abc import ABC, abstractmethod
 import logging
+from typing import Any, Dict, List, Optional
 
 import aiohttp
+from aiohttp import ClientSession
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -14,17 +16,17 @@ class Engine(ABC):
     with Metronet IESS cloud platform.
     """
 
-    def __init__(self, zipcode):
+    def __init__(self, zipcode: int):
         """Init for data."""
-        self.lookup = {}
-        self.zipcode = zipcode
-        self.providers = []
-        self.provider = None
-        self.requires_provider = True
+        self.lookup: Dict[str, List[Any]] = {}
+        self.zipcode: int = zipcode
+        self.providers: List[Any] = []
+        self.provider: Optional[int] = None
+        self.requires_provider: bool = True
         super().__init__()
 
     @staticmethod
-    async def get_response(url, session):
+    async def get_response(url, session: ClientSession) -> Optional[str]:
         """Get page text."""
         try:
             page = await session.request(method="GET", url=url)
@@ -35,11 +37,11 @@ class Engine(ABC):
         return None
 
     @abstractmethod
-    async def load_channels(self, session):
+    async def load_channels(self, session: ClientSession):
         """Load channels from selected provider."""
 
     @abstractmethod
-    async def load_providers(self, session):
+    async def load_providers(self, session: ClientSession):
         """Load Provider list."""
 
     def add_channel_mapping(self, name, is_hd, lcn):
@@ -63,5 +65,5 @@ class Engine(ABC):
                     self.lookup[name][0] = lcn
 
     @abstractmethod
-    def normalize_channel_name(self, channel):
+    def normalize_channel_name(self, channel: str) -> str:
         """Normalize channel name."""
